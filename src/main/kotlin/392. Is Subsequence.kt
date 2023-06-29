01180dcd16d9eca35b08baab1d0230235f307912
Mohib <gazi.mohib@brainstation-23.com>
2023-06-29 12:30:02 +0100

    solved is subsequence

---
+++ src/main/kotlin/392. Is Subsequence.kt
@@ -0,0 +1,12 @@
+fun isSubsequence(s: String, t: String): Boolean {
+    var si = 0
+    var ti = 0
+
+    while (si < s.length && ti < t.length){
+        if(s[si] == t[ti]){
+            si++
+        }
+        ti++
+    }
+    return si == s.length
+}
