bb4f2230a65bf2510a4c495bd7a695df93204f10
Mohib <gazi.mohib@brainstation-23.com>
2023-11-15 00:36:13 +0000

    added java class, solution

--- src/main/kotlin/Solution.java
+++ src/main/kotlin/Solution.java
@@ -1,5 +1,10 @@
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.lang.Math.max;
+import static java.lang.Math.sqrt;
 
 class Pair {
     int row;
@@ -191,7 +196,7 @@ public class Solution {
         while (!queue.isEmpty()){
             var node = queue.poll();
 
-            maxTime = Math.max(node.time, maxTime);
+            maxTime = max(node.time, maxTime);
 
             for (int[] dir : directions){
                 int x = node.row + dir[0];
@@ -351,9 +356,362 @@ public class Solution {
         return result;
     }
 
-    public static void main(String[] args) {
-        var solution = new Solution();
+    public static String timeConversion(String s) {
+        String type = s.substring(8);
 
-        System.out.println(solution.combinationSum2(new int[]{10,1,2,7,6,1,5},8));
+        int hour = Integer.parseInt(s.substring(0,2));
+        int newHour = 0;
+
+        if(type.equals("PM") && hour < 12){
+            newHour = hour + 12;
+        }else if(type.equals("AM") && hour == 12){
+            newHour = 0;
+        }else{
+            return s.substring(0,8);
+        }
+
+        return String.format("%02d" + s.substring(2,8), newHour);
+    }
+
+    public int lonelyinteger(List<Integer> nums) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        for (int a : nums){
+            if (countMap.containsKey(a)){
+                countMap.put(a, countMap.get(a) + 1);
+            }else{
+                countMap.put(a, 1);
+            }
+        }
+
+        AtomicInteger result = new AtomicInteger();
+        countMap.forEach((key, value) -> {
+            if(value == 1)
+                result.set(key);
+        });
+
+        return result.get();
+    }
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for (int i = 0, j = arr.size() - 1; i<arr.size(); i++, j--){
+            sum1 += arr.get(i).get(i);
+            sum2 += arr.get(i).get(j);
+        }
+
+        return Math.abs(sum1 - sum2);
+    }
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+
+        int mx = arr.stream()
+                .max(Integer::compare)
+                .orElse(0);
+
+        int[] counting = new int[mx + 1];
+
+        for (int a : arr){
+            counting[a] = counting[a] + 1;
+        }
+
+        return Arrays.stream(counting)
+                .boxed()
+                .collect(Collectors.toList());
+
+    }
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        Map<Integer, Integer> colMaxMap = new HashMap<>();
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        for (int i = 0; i < m; i++){
+            int colMax = Integer.MIN_VALUE;
+            for(int j = 0; j < n; j++){
+                if (matrix[j][i] > colMax){
+                    colMax = matrix[j][i];
+                }
+            }
+            colMaxMap.put(i, colMax);
+        }
+
+        for (int i = 0; i < n; i++){
+            int rowMin = Integer.MAX_VALUE;
+            int rowIdx = 0;
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] < rowMin){
+                    rowMin = matrix[i][j];
+                    rowIdx = j;
+                }
+            }
+
+            if(colMaxMap.containsKey(rowIdx) && colMaxMap.get(rowIdx) == rowMin){
+                result.add(rowMin);
+            }
+        }
+
+        return result;
+    }
+
+    public int countNumbersWithUniqueDigits(int n) {
+       int[] res = new int[9];
+       res[0] = 1;
+       res[1] = 10;
+
+       for(int i = 2; i < 10; i++){
+           int mult = 9;
+
+           for(int j = 9; j > 9 - i + 1; j--){
+               mult *= j;
+           }
+
+           res[i] = mult + res[i-1];
+       }
+
+       return res[n];
+    }
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right){
+            int mid = (left + right)/2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if(nums[left] <= nums[mid]){
+                if(nums[left] <= target && target < nums[mid]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }else{
+                if(nums[mid] < target && target <= nums[right]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public int flippingMatrix(List<List<Integer>> matrix) {
+       int sum = Integer.MIN_VALUE;
+       int n = matrix.size();
+
+       for (int i = 0; i < n/2; i++){
+           for (int j = 0; j< n/2; j++){
+               sum += Math.max(
+                       Math.max(matrix.get(i).get(j), matrix.get(i).get((2 * n - 1) - j)),
+                       Math.max(matrix.get((2 * n - 1) - i).get(j), matrix.get((2 * n - 1) - i).get((2 * n - 1) - j))
+               );
+           }
+       }
+
+       return sum;
+    }
+
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        int count = 1;
+        int idx = 0;
+
+        for(int i = 1; i < intervals.length; i++){
+            if(intervals[idx][1] < intervals[i][1]){
+                count++;
+                idx = i;
+            }
+        }
+        return count;
+    }
+
+    public int diagonalPrime(int[][] nums) {
+        int maxPrime = 0;
+
+        for(int i = 0; i<nums.length; i++){
+            if(isPrime(nums[i][i])){
+                maxPrime = Math.max(maxPrime, nums[i][i]);
+            }
+
+            if(isPrime(nums[i][nums.length - 1 - i])){
+                maxPrime = Math.max(maxPrime, nums[i][nums.length - 1 - i]);
+            }
+        }
+
+        return maxPrime;
+    }
+
+    private boolean isPrime(int num){
+        if(num <= 1)
+            return false;
+
+        for(int i = 2; i <= sqrt(num); i++){
+            if(num * i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static String caesarCipher(String s, int k) {
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 0; i<s.length(); i++){
+            int value = 0;
+
+            if(s.charAt(i) >= 'a' && s.charAt(i)<= 'z'){
+                value = s.charAt(i) - 97;
+                result.insert(i,(char) ((value + k ) % 26 + 97));
+            }else if(s.charAt(i) >= 'A' && s.charAt(i)<= 'Z'){
+                value = s.charAt(i) - 65;
+                result.insert(i,(char) ((value + k ) % 26 + 65));
+            }else{
+                result.insert(i,s.charAt(i));
+            }
+
+        }
+
+        return result.toString();
+    }
+
+    public static String gridChallenge(List<String> grid) {
+        List<String> sortedGrid = new ArrayList<>();
+
+        for (String row: grid){
+            char[] rowCharArr = row.toCharArray();
+            Arrays.sort(rowCharArr);
+
+            sortedGrid.add(new String(rowCharArr));
+        }
+
+        for (int i = 0; i < sortedGrid.get(0).length(); i++){
+            for (int j = 0; j< sortedGrid.size() - 1; j++){
+                if(sortedGrid.get(j).charAt(i) > sortedGrid.get(j + 1).charAt(i)){
+                    return "NO";
+                }
+            }
+        }
+
+        return "YES";
+    }
+
+    public static void minimumBribes(List<Integer> q) {
+        int totalBribes = 0;
+
+        for(int i = 0; i < q.size() ; i++){
+            int distance = q.get(i) - i - 1;
+            int bribe = 0;
+
+            if(distance > 0){
+                int j = i;
+                while(j < q.size() - 1 && q.get(j) > q.get(j + 1)){
+                    Collections.swap(q, j,j + 1);
+                    bribe++;
+                    j++;
+                }
+            }else if(distance < 0){
+                int j = i;
+
+                while (j > 0 && q.get(j) < q.get(j - 1)){
+                    Collections.swap(q, j, j -1);
+                    bribe++;
+                    j++;
+                }
+            }
+
+            if(bribe > 2){
+                System.out.println("Too chaotic");
+                return;
+            }
+            totalBribes += bribe;
+        }
+
+        System.out.println(totalBribes);
+
+    }
+
+    public static int truckTour(List<List<Integer>> petrolpumps) {
+
+        for(int i = 0; i< petrolpumps.size(); i++){
+            int fuel = petrolpumps.get(i).get(0) - petrolpumps.get(i).get(1);
+
+            if(fuel < 0){
+                continue;
+            }
+            int start = i;
+
+            while(fuel >= 0){
+                start = (start + 1) % petrolpumps.size();
+                fuel += (petrolpumps.get(start).get(0) - petrolpumps.get(start).get(1));
+                if(start == i){
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public int[] findDiagonalOrder(List<List<Integer>> nums) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        int size = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 0; j < nums.get(i).size(); j++){
+                List<Integer> list = map.getOrDefault(i + j, new ArrayList<Integer>());
+                list.add(0, nums.get(i).get(j));
+                map.put(i + j, list);
+                size++;
+            }
+        }
+
+        int[] arr = new int[size];
+
+        int idx = 0;
+        for (int i = 0; i < map.size(); i++){
+            for (int n : map.get(i)){
+                arr[idx++] = n;
+            }
+        }
+
+        return arr;
+    }
+
+    public long makeIntegerBeautiful(long n, int target) {
+
+        if(sumOfDigit(n) <= target)
+            return 0;
+        int x = 1;
+        while(sumOfDigit(n + x) > target){
+            x++;
+            System.out.println(x);
+        }
+
+        return x;
+    }
+
+    private int sumOfDigit(long n){
+        int sum = 0;
+        while(n != 0){
+            sum += n%10;
+            n /= 10;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.makeIntegerBeautiful(467, 6));
     }
 }
