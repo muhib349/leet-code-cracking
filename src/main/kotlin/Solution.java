3d9b457f1e407209074d0fd642bf30d60bb31da6
muhib349 <muhibgazi@gmail.com>
2024-02-08 17:24:45 +0000

    279. Perfect Squares

--- src/main/kotlin/Solution.java
+++ src/main/kotlin/Solution.java
@@ -862,6 +862,25 @@ public class Solution {
         return ans;
     }
 
+    /**
+     * 279. Perfect Squares
+     * @param n
+     * @return
+     */
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j*j <= i; j++){
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
 
